72d3f22cda57174426bea5daee32be068a68b147
abdullah a7med <mr.abdullah.ahmed.saeed.study1@gmail.com>
2025-06-26 18:24:28 +0300

    upload file of some data type

---
+++ day2/1_primitive_Data_Types.py
@@ -0,0 +1,74 @@
+# lesson -> 1 in part-2
+#  |=======================|
+#  |= primitive Data type =|
+#  |=======================|
+
+
+# as we know => len() function , used to know number of elemint
+# Ex:
+print(len("abdullah"))  # => 8
+
+# ? now i have aquestion : what if i change (abdullah) by 12345:
+#! print(len(1234))   => Type Error
+# TypeError: object of type 'int' has no len()
+
+# we cant use => int data type with len <=
+
+# ./////////////////////////////////////////////////////////////////////
+# .////////////////// let's know some of => Data Types <= //////////////
+# ./////////////////////////////////////////////////////////////////////
+
+
+# String:
+# Strings are ordered sequences
+# we type string inner => "" <=
+
+# EX:
+"Ninja"
+
+# becouse string -> are ordered sequences
+# i can take or pull any char
+# "MyText"[Char_possition] -> start from 0
+# EX:
+print("HERo"[0]) #H
+
+# that way call ==> subscript
+
+# * Note => any thing inner -> "" <- | are string
+
+sum = "12" + "1"
+# if 12 + 1 => was integer the result will be : => 13
+# but becouse there are string result is => 121
+
+print(sum) #121
+
+
+# =====================/separator/=================
+
+## integer => is a whole number , without decimal components.
+# EX :
+# 1,2,3,..... | -1,-2,..... | 0 | 
+
+n1 = 12 + 1
+print(n1) # 13
+
+
+# =====================/separator/=================
+
+# float => any number with decimal.
+# EX:
+# ====> 1.3 , 5.9 ,0.1, ..... | -1.54 |
+
+n2 = 12.1 + 0.9
+
+print(n2) # 13.0
+
+# =====================/separator/=================
+
+# Boolean
+#  we have 2 => True / False
+
+
+
+
+
