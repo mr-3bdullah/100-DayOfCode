473572d37e413d9db837278c95b72219a5124a7c
abdullah ahmed <mr.abdullah.ahmed.saeed.study1@gmail.com>
2025-07-01 23:22:28 +0300

    simble exercise on data type

---
+++ day2/3_exercise-data-type.py
@@ -0,0 +1,20 @@
+# wellcom in simble exercise
+
+# -> write a program  that adds the digits in a 2 didit number.
+# if the input eas 35 , then the output should be ==> 3+5=5 
+
+# /././././///////////////// solution //////////////////
+
+
+# we use the sclicing.
+
+
+my_num = (input("type your num: ")) #input number
+
+
+num1=int(my_num[0]) #conver & sclicing my number
+
+num2=int(my_num[1])
+
+res = num1+num2
+print(res)
